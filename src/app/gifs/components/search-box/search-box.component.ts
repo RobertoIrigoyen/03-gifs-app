4eb323165de834ff997e94282652aacc16b8e2c4
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-05-30 19:53:31 +0200

    Preparacion proyecto e inicio de maquetacion

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  template: `
+  <h5>Buscar:</h5>
+  <input type="text" class="form-control" name="" id="" placeholder="Buscar gifs">
+  `,
+  styleUrls: ['./search-box.component.scss']
+})
+export class SearchBoxComponent {
+
+}
