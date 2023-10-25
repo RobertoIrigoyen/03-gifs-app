90fcad80b1c30511eaee8001f006de126c135da0
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-10-25 12:12:42 +0200

    Inicio GifServices

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-box',
   template: `
   <h5>Buscar:</h5>
-  <input type="text" class="form-control" name="" id="" placeholder="Buscar gifs">
+  <input type="text" class="form-control" name="" id="" (keyup.enter)="searchTag()" placeholder="Buscar gifs" #textInput>
   `,
   styleUrls: ['./search-box.component.scss']
 })
 export class SearchBoxComponent {
 
+  @ViewChild('textInput')
+  public tagInput!: ElementRef<HTMLInputElement>
+
+  constructor(private gifsService: GifsService) {
+
+  }
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = ''
+  }
 }
